81886a12e5bb87a84a0815db36459c5be9762aaf
Artem Kolin <artyom.kolin@eltex.loc>
2019-06-13 15:40:04 +0700

    Sections 8 Volumes - ready

--- scripts/playground-test.sh
+++ scripts/playground-test.sh
@@ -8,3 +8,7 @@ docker run -d -P tomcat
 docker build -t your-docker-id/vim:1.2 files/01-build-vim
 docker build -t your-docker-id/vim:1.3 files/02-build-vim-ping-cmd
 docker build -t your-docker-id/vim:1.4 files/03-build-vim-ping-ep
+docker run -d -P -v testvolume:/myvolume nginx:1.9.4
+docker run -i -t -v /data/src:/test/src nginx:1.9.4
+mkdir target
+docker run -d -v "$(pwd)"/target:/app nginx
