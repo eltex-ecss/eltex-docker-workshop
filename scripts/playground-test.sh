60d1d933274ca8a59c144221d594073da2d25be6
Artem Kolin <artyom.kolin@eltex.loc>
2019-06-13 09:42:14 +0700

    Sections 6 - ready

--- scripts/playground-test.sh
+++ scripts/playground-test.sh
@@ -5,3 +5,6 @@ docker run ubuntu:14.04 ps aux
 docker run -i -t ubuntu /bin/bash
 docker run -d ubuntu ping 127.0.0.1 -c 50
 docker run -d -P tomcat
+docker build -t your-docker-id/vim:1.2 files/01-build-vim
+docker build -t your-docker-id/vim:1.3 files/02-build-vim-ping-cmd
+docker build -t your-docker-id/vim:1.4 files/03-build-vim-ping-ep
